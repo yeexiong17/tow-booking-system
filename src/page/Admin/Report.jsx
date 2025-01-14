96a2092ede1ecf8df45722ca41eedf5b3f0f6665
lenovo/yeexiong17 <wyeexiong17@gmail.com>
2025-01-14 19:55:12 +0800

    feat: Update report page for admin

--- src/page/Admin/Report.jsx
+++ src/page/Admin/Report.jsx
@@ -8,37 +8,28 @@ import classes from '../../styles/ReportTable.module.css'
 
 const data = [
     {
-        name: 'Athena Weissnat',
-        company: 'Little - Rippin',
-        email: 'Elouise.Prohaska@yahoo.com',
+        id: 1,
+        date: '22/03/2025',
     },
     {
-        name: 'Deangelo Runolfsson',
-        company: 'Greenfelder - Krajcik',
-        email: 'Kadin_Trantow87@yahoo.com',
+        id: 2,
+        date: '10/01/2025',
     },
     {
-        name: 'Danny Carter',
-        company: 'Kohler and Sons',
-        email: 'Marina3@hotmail.com',
-    },
-    {
-        name: 'Trace Tremblay PhD',
-        company: 'Crona, Aufderhar and Senger',
-        email: 'Antonina.Pouros@yahoo.com',
-    },
+        id: 3,
+        date: '17/01/2025',
+    }
 ]
 
 const Report = () => {
 
     const [value, setValue] = useState(null)
-
-    const [scrolled, setScrolled] = useState(false);
+    const [scrolled, setScrolled] = useState(false)
 
     const rows = data.map((row) => (
         <Table.Tr key={row.name}>
-            <Table.Td>{row.name}</Table.Td>
-            <Table.Td>{row.email}</Table.Td>
+            <Table.Td>{row.id}</Table.Td>
+            <Table.Td>{row.date}</Table.Td>
             <Table.Td>
                 <Button>Generate Report</Button>
             </Table.Td>
@@ -64,8 +55,8 @@ const Report = () => {
                 <Table miw={700}>
                     <Table.Thead>
                         <Table.Tr>
-                            <Table.Th>Name</Table.Th>
-                            <Table.Th>Email</Table.Th>
+                            <Table.Th>No.</Table.Th>
+                            <Table.Th>Date</Table.Th>
                             <Table.Th></Table.Th>
                         </Table.Tr>
                     </Table.Thead>
