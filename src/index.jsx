de71fe6e17b4b771672a118ba8d04e49de645569
lenovo/yeexiong17 <wyeexiong17@gmail.com>
2025-01-14 19:47:54 +0800

    feat: Added report page for admin

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,7 @@ import '@mantine/core/styles.css'
 import '@mantine/notifications/styles.css'
 import '@mantine/carousel/styles.css'
 import '@mantine/charts/styles.css'
+import '@mantine/dates/styles.css'
 
 import App from './App'
 import { AuthProvider } from './Context'
--- src/page/Admin/Report.jsx
+++ src/page/Admin/Report.jsx
@@ -1,9 +1,77 @@
+import { useState } from 'react'
 import CommonLayout from '../../components/CommonLayout'
+import { DatePickerInput } from '@mantine/dates'
+import { Button, Flex, Space } from '@mantine/core'
+import { ScrollArea, Table } from '@mantine/core'
+import classes from '../../styles/ReportTable.module.css'
+
+
+const data = [
+    {
+        name: 'Athena Weissnat',
+        company: 'Little - Rippin',
+        email: 'Elouise.Prohaska@yahoo.com',
+    },
+    {
+        name: 'Deangelo Runolfsson',
+        company: 'Greenfelder - Krajcik',
+        email: 'Kadin_Trantow87@yahoo.com',
+    },
+    {
+        name: 'Danny Carter',
+        company: 'Kohler and Sons',
+        email: 'Marina3@hotmail.com',
+    },
+    {
+        name: 'Trace Tremblay PhD',
+        company: 'Crona, Aufderhar and Senger',
+        email: 'Antonina.Pouros@yahoo.com',
+    },
+]
 
 const Report = () => {
+
+    const [value, setValue] = useState(null)
+
+    const [scrolled, setScrolled] = useState(false);
+
+    const rows = data.map((row) => (
+        <Table.Tr key={row.name}>
+            <Table.Td>{row.name}</Table.Td>
+            <Table.Td>{row.email}</Table.Td>
+            <Table.Td>
+                <Button>Generate Report</Button>
+            </Table.Td>
+        </Table.Tr>
+    ))
+
     return (
         <CommonLayout>
-            Hello
+            <p className='font-bold text-2xl'>Report</p>
+
+            <Space h={20} />
+            <div className='w-2/6'>
+                <DatePickerInput
+                    label="Pick date"
+                    placeholder="Pick date"
+                    value={value}
+                    onChange={setValue}
+                />
+            </div>
+
+            <Space h={20} />
+            <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+                <Table miw={700}>
+                    <Table.Thead>
+                        <Table.Tr>
+                            <Table.Th>Name</Table.Th>
+                            <Table.Th>Email</Table.Th>
+                            <Table.Th></Table.Th>
+                        </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>{rows}</Table.Tbody>
+                </Table>
+            </ScrollArea>
         </CommonLayout>
 
     )
